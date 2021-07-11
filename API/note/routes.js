1667d08fba6540e37ab1da4e8acf0fe042454925
mahmood114 <mahmoodalmohri@gmail.com>
2021-07-11 17:10:32 +0300

    bug fixes

--- API/note/routes.js
+++ API/note/routes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const {
   noteFetch,
   fetchNote,
   noteUpdate,
   noteDelete,
-} = require('./controllers');
-const multer = require('multer');
+} = require("./controllers");
+const multer = require("multer");
 
 const router = express.Router();
 
 // Multer
 const storage = multer.diskStorage({
-  destination: './media',
+  destination: "./media",
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
@@ -19,22 +19,22 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // param middleware
-router.param('noteId', async (req, res, next, noteId) => {
+router.param("noteId", async (req, res, next, noteId) => {
   const note = await fetchNote(noteId, next);
   if (note) {
     req.note = note;
     next();
   } else {
-    const error = new Error('note Not Found!');
+    const error = new Error("note Not Found!");
     error.status(404);
     next(error);
   }
 });
 
-router.get('/', noteFetch);
+router.get("/", noteFetch);
 
-router.put('/:noteId', upload.single('image'), noteUpdate);
+router.put("/:noteId", upload.single("image"), noteUpdate);
 
-router.delete('/:noteId', noteDelete);
+router.delete("/:noteId", noteDelete);
 
 module.exports = router;
